bdf30d99a1a62a586e23db9db5443bee98174090
ritesh8380 <pritesh8380@gmail.com>
2026-07-10 11:26:17 +0530

    The hello progragram dissection and comments information for each line

--- hello.cpp
+++ hello.cpp
@@ -11,9 +11,9 @@ int main(){ //this is the main function where the programme starts executing//
     //standard namespace becomes problem when we have multiple libraries with same function names and we use them in the same program for example  c++ libraries like <fstream> and <iomanip>//
     
     return 0;
-    //This returns 0 to the operating system which indicates that the program has executed successfully//
+    /*This returns 0 to the operating system which indicates that the program has executed successfully*/
 
-    //we can still use return 0,1,2,3,4,5,6,7,8,9 but 0 is used to indicate that the program has executed successfully and any other number indicates that the program has failed to execute successfully even though it may have run without crashing but will give errors when run in a production environment (production environment means the environment where the program will be deployed for actual use example calculator if 0 means success but if any other then it means failure calculator stops)//
+    /*we can still use return 0,1,2,3,4,5,6,7,8,9 but 0 is used to indicate that the program has executed successfully and any other number indicates that the program has failed to execute successfully even though it may have run without crashing but will give errors when run in a production environment (production environment means the environment where the program will be deployed for actual use example calculator if 0 means success but if any other then it means failure calculator stops)*/
     
     //(This Returns not for IDE they are for operating system to know the status of the program execution)//
 }
